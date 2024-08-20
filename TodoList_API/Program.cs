33cc3aa09fe5afa1a0d8f06e4354aafbbe7161f7
vikkoooo <vikkolundberg@gmail.com>
2024-08-20 15:06:42 +0200

    remove swagger from project

--- TodoList_API/Program.cs
+++ TodoList_API/Program.cs
@@ -14,24 +14,11 @@ namespace TodoList_API
 			builder.Services.AddDbContext<TodoContext>(opt => // Adds the database context to the DI container.
 			opt.UseInMemoryDatabase("TodoList")); // Specifies that the database context will use an in-memory database.
 
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
-
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
-
 			app.UseHttpsRedirection();
-
 			app.UseAuthorization();
-
-
 			app.MapControllers();
 
 			app.Run();
